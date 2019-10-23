9d2a23d7764429582f636bed8ebe06b4e6634990
AK <alexey1312ru@gmail.com>
2019-10-23 20:37:02 +0700

    Initial Commit

---
+++ musicPlayerTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicPlayerTestApp
+//
+//  Created by Admin on 23.10.2019.
+//  Copyright © 2019 Aleksei Kakoulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
