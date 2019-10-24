a0be2f0e005404d011afe5e7484ebeabf669327e
AK <alexey1312ru@gmail.com>
2019-10-24 22:46:03 +0700

    add Image refactoring functions play, stop, pause

---
+++ musicPlayerTestApp/MusicPlayer.swift
@@ -0,0 +1,48 @@
+//
+//  MusicPlayer.swift
+//  musicPlayerTestApp
+//
+//  Created by Admin on 24.10.2019.
+//  Copyright © 2019 Aleksei Kakoulin. All rights reserved.
+//
+
+import AVFoundation
+
+class MusicPlayer {
+    
+    var player = AVPlayer()
+    
+    func initPlayer(url : String) {
+        guard let url = URL.init(string: url) else { return }
+        let playerItem = AVPlayerItem.init(url: url)
+        player = AVPlayer.init(playerItem: playerItem)
+        playAudioBackground()
+    }
+    
+    func playAudioBackground() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [.mixWithOthers, .allowAirPlay])
+            print("Playback OK")
+            try AVAudioSession.sharedInstance().setActive(true)
+            print("Session is Active")
+        } catch {
+            print(error)
+        }
+    }
+    
+    func pause(){
+        player.pause()
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func stop() {
+        player.seek(to: CMTime.zero)
+    }
+}
+
+
+
+
--- musicPlayerTestApp/ViewController.swift
+++ musicPlayerTestApp/ViewController.swift
@@ -9,90 +9,65 @@
 import UIKit
 import AVFoundation
 
-
 class ViewController: UIViewController {
     
-    class MusicPlayer {
-        public static var instance = MusicPlayer()
-        var player = AVPlayer()
-        
-        func initPlayer(url : String) {
-            guard let url = URL.init(string: url) else { return }
-            let playerItem = AVPlayerItem.init(url: url)
-            player = AVPlayer.init(playerItem: playerItem)
-            playAudioBackground()
-        }
-        
-        func playAudioBackground() {
-            do {
-                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [.mixWithOthers, .allowAirPlay])
-                print("Playback OK")
-                try AVAudioSession.sharedInstance().setActive(true)
-                print("Session is Active")
-            } catch {
-                print(error)
-            }
-        }
-        
-        func pause(){
-            player.pause()
-        }
-        
-        func play() {
-            player.play()
-        }
-        
-        func stop() {
-            player.seek(to: CMTime.zero)
-        }
-    }
-    
-    
-    
     var player = MusicPlayer()
+    //let URL
     let tasteURL = "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3"
+    
+    var button = true
+    
+    //image buttons
+    let imagePlay = UIImage(#imageLiteral(resourceName: "ic_play_arrow_48px")).withTintColor(.black)
+    let imagePause = UIImage(#imageLiteral(resourceName: "ic_pause_circle_outline_48px")).withTintColor(.black)
+    let imageStop = UIImage(#imageLiteral(resourceName: "ic_stop_48px")).withTintColor(.black)
+    
+    //buttons
+    let buttonPlay = UIButton(frame: CGRect(x: 75, y: 300, width: 75, height: 75))
+    let buttonStop = UIButton(frame: CGRect(x: 200, y: 300, width: 75, height: 75))
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         player.initPlayer(url: tasteURL)
         
-        let buttonPlay = UIButton(frame: CGRect(x: 50, y: 300, width: 100, height: 50))
-        buttonPlay.setTitle("Play music", for: .normal)
+        buttonPlay.setBackgroundImage(imagePlay, for: .normal)
         buttonPlay.setTitleColor(.black, for: .normal)
-        buttonPlay.backgroundColor = .green
-        buttonPlay.addTarget(self, action: #selector(buttonActionPlay), for: .touchUpInside)
+        buttonPlay.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        buttonPlay.addTarget(self, action: #selector(buttonActionPlayOrPause), for: .touchUpInside)
         self.view.addSubview(buttonPlay)
         
-        let buttonPause = UIButton(frame: CGRect(x: 200, y: 300, width: 120, height: 50))
-        buttonPause.setTitle("Pause music", for: .normal)
-        buttonPause.setTitleColor(.black, for: .normal)
-        buttonPause.backgroundColor = .yellow
-        buttonPause.addTarget(self, action: #selector(buttonActionPause), for: .touchUpInside)
-        self.view.addSubview(buttonPause)
-        
-        let buttonStop = UIButton(frame: CGRect(x: 120, y: 400, width: 100, height: 50))
-        buttonStop.setTitle("Stop", for: .normal)
+        buttonStop.setBackgroundImage(imageStop, for: .normal)
         buttonStop.setTitleColor(.black, for: .normal)
-        buttonStop.backgroundColor = .red
+        buttonStop.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         buttonStop.addTarget(self, action: #selector(buttonActionStop), for: .touchUpInside)
         self.view.addSubview(buttonStop)
-        
     }
     
-    @objc func buttonActionPlay(sender: UIButton!) {
+    @objc func buttonActionPlayOrPause(sender: UIButton!) {
         print("buttonActionPlay tapped")
-        player.play()
-    }
-    
-    @objc func buttonActionPause(sender: UIButton!) {
-        print("buttonActionPause tapped")
-        player.pause()
+        button.toggle()
+        
+        if button {
+            player.pause()
+            buttonPlay.setBackgroundImage(imagePlay, for: .normal)
+        } else {
+            player.play()
+            buttonPlay.setBackgroundImage(imagePause, for: .normal)
+        }
     }
     
     @objc func buttonActionStop(sender: UIButton!) {
         print("buttonActionStop tapped")
         player.stop()
         player.pause()
+        
+        button = false
+        if button {
+            buttonPlay.setBackgroundImage(imagePause, for: .normal)
+        } else {
+            button = true
+            buttonPlay.setBackgroundImage(imagePlay, for: .normal)
+        }
     }
 }
