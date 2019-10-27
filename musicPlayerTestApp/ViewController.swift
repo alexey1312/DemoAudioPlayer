af362de9612ed89e44f1bacfe4d61492a746dfb4
AK <alexey1312ru@gmail.com>
2019-10-27 19:17:02 +0700

    add contrite

--- musicPlayerTestApp/ViewController.swift
+++ musicPlayerTestApp/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import AVFoundation
 import SnapKit
-import MediaPlayer
 
 class ViewController: UIViewController {
     
@@ -28,11 +27,11 @@ class ViewController: UIViewController {
     var timer: Timer? = nil
     
     //image buttons
-    let imagePlay = UIImage(#imageLiteral(resourceName: "ic_play_arrow_48px")).withTintColor(.black)
-    let imagePause = UIImage(#imageLiteral(resourceName: "ic_pause_48px")).withTintColor(.black)
-    let imageStop = UIImage(#imageLiteral(resourceName: "ic_stop_48px")).withTintColor(.black)
-    let imageVolumeUP = UIImage(#imageLiteral(resourceName: "ic_volume_up_48px")).withTintColor(.black)
-    let imageVolumeDOWN = UIImage(#imageLiteral(resourceName: "ic_volume_off_48px")).withTintColor(.black)
+    let imagePlay = UIImage(#imageLiteral(resourceName: "ic_play_arrow_48px")).withTintColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
+    let imagePause = UIImage(#imageLiteral(resourceName: "ic_pause_48px")).withTintColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
+    let imageStop = UIImage(#imageLiteral(resourceName: "ic_stop_48px")).withTintColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
+    let imageVolumeUP = UIImage(#imageLiteral(resourceName: "ic_volume_up_48px")).withTintColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
+    let imageVolumeDOWN = UIImage(#imageLiteral(resourceName: "ic_volume_off_48px")).withTintColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1))
     
     //texr field
     let textField = UITextField(frame: CGRect(x: 25, y: 100, width: 325, height: 50))
@@ -44,48 +43,79 @@ class ViewController: UIViewController {
     let progressBar = UIProgressView(frame: CGRect(x: 90, y: 280, width: 200, height: 75))
     
     //buttons
-    let buttonPlayOrPause = UIButton(frame: CGRect(x: 90, y: 300, width: 75, height: 75))
-    let buttonStop = UIButton(frame: CGRect(x: 220, y: 300, width: 75, height: 75))
-    let buttonMute = UIButton(frame: CGRect(x: 150, y: 470, width: 75, height: 75))
+    let buttonPlayOrPause = UIButton(frame: CGRect(x: 90, y: 300, width: 50, height: 50))
+    let buttonStop = UIButton(frame: CGRect(x: 220, y: 300, width: 50, height: 50))
+    let buttonMute = UIButton(frame: CGRect(x: 150, y: 470, width: 50, height: 50))
     
     //text volume
     let textLabelVolume = UITextView(frame: CGRect(x: 155, y: 380, width: 200, height: 75))
     
     //volume slider
     let volumeSlider = UISlider(frame: CGRect(x: 90, y: 400, width: 200, height: 75))
-
+    
+    //hide keyboard  afte tap
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.view.endEditing(true) // Скрытие клавиатуры вызванной для любого объекта
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //test url init
         player.initPlayer(url: tasteURL)
         
-        textField.backgroundColor = .yellow
+        //init textField
+        textField.backgroundColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        textField.layer.borderColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         textField.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         textField.text = "add your URL mp3"
-        textField.borderStyle = .line
+        textField.borderStyle = .roundedRect
         textField.clearsOnBeginEditing = true
         textField.clearButtonMode = UITextField.ViewMode.whileEditing
         self.view.addSubview(textField)
+        textField.snp.makeConstraints { (make) in
+            make.top.equalTo(view).offset(75)
+            make.left.equalTo(view).offset(25)
+            make.right.equalTo(view).offset(-25)
+            make.bottom.equalTo(view).offset(-550)
+        }
         
+        //init textLabelProgress and progress
         textLabelProgress.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        textLabelProgress.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         textLabelProgress.text = "0 : 0"
-
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            
             self.progressBar.progress = Float(CMTimeGetSeconds(self.player.player.currentTime()) / CMTimeGetSeconds(self.player.availableDuration()))
             self.textLabelProgress.text = "\(Int(CMTimeGetSeconds(self.player.player.currentTime()))) : \(Int(CMTimeGetSeconds(self.player.availableDuration())))"
         }
-    
-        self.view.addSubview(textLabelProgress)
-        self.view.addSubview(progressBar)
         
+        self.view.addSubview(textLabelProgress)
+        textLabelProgress.snp.makeConstraints { (make) in
+            make.size.greaterThanOrEqualTo(textField)
+//            make.top.equalTo(textField).offset(50)
+//            make.left.equalTo(view).offset(200)
+//            make.right.equalTo(view).offset(-200)
+            make.center.equalTo(view)
+        }
+        
+        self.view.addSubview(progressBar)
+        progressBar.snp.makeConstraints { (make) in
+            make.top.equalTo(view).offset(250)
+            make.left.equalTo(view).offset(25)
+            make.right.equalTo(view).offset(-25)
+            make.centerX.equalTo(view)
+        }
+        
+        //init buttonPlayOrPause
         buttonPlayOrPause.setBackgroundImage(imagePlay, for: .normal)
         buttonPlayOrPause.setTitleColor(.black, for: .normal)
         buttonPlayOrPause.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         buttonPlayOrPause.addTarget(self, action: #selector(buttonActionPlayOrPause), for: .touchUpInside)
         self.view.addSubview(buttonPlayOrPause)
         
+        //init buttonPlayOrPause
         buttonStop.setBackgroundImage(imageStop, for: .normal)
         buttonStop.setTitleColor(.black, for: .normal)
         buttonStop.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
@@ -97,7 +127,7 @@ class ViewController: UIViewController {
         buttonMute.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         buttonMute.addTarget(self, action: #selector(buttonMuteAction), for: .touchUpInside)
         self.view.addSubview(buttonMute)
-              
+        
         textLabelVolume.textColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         textLabelVolume.text = "Volume 15%"
         self.view.addSubview(textLabelVolume)
@@ -178,5 +208,4 @@ class ViewController: UIViewController {
             buttonMute.setBackgroundImage(imageVolumeUP, for: .normal)
         }
     }
-    
 }
